6dd20dca9d9c48c3ff0d485f9805baa42ca397d7
Luciano Corniglione <luciano.corniglione@gmail.com>
2017-06-30 15:44:45 -0300

    Json parsed

--- integration-api/integration-api.py
+++ integration-api/integration-api.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from application_properties import *
 from sqlite_connector import new_client, sqlite_connect, get_database
 from odoo_connector import odoo_connect
+import json
 
 app = Flask(__name__)
 
@@ -22,11 +23,34 @@ def create_client():
 
 @app.route('/new_lead', methods=['POST'])
 def insert_lead():
-    page_name = request.form.get('page_name')
+    page_name = request.json.get('page_name')
     odoo_connection = odoo_connect(get_database(page_name.lower()))
+    json = request.get_json()
+    print(json)
+    odooJson = transformToOdooJson(json)
+    print(odooJson)
     return str(odoo_connection)
 
+def transformToOdooJson(json):
+    for key, values in json.copy().items():
+        k = getOdooKey(key)
+        if k != None:
+            if k == key:
+                json[k] = json[key]
+            else:
+                json[k] = json[key]
+                del json[key]
+        else:
+            del json[key]
+    return json
 
+def getOdooKey(formKey):
+    with open('./odoo_mappings.json') as json_data:
+        odooMapping = json.load(json_data)
+
+    for key, values in odooMapping.items():
+        if formKey == key:
+            return values
 
 
 if __name__ == '__main__':
